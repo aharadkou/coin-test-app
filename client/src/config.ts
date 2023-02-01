9f8e640652caa9b74debd99dbfbeeacf598a9b64
DaDaYa <you@example.com>
2023-02-01 17:25:53 +0300

    fix config

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,4 +5,4 @@ export const TRANSACTIONS_URL = `${BASE_URL}/transactions`;
 export const USER_INFO_URL = `${BASE_URL}/auth/user-info`;
 export const USERS_URL = `${BASE_URL}/users`;
 
-export const AUTH_APP_UI_URL = 'http://localhost:3000' || process.env.AUTH_APP_UI_URL;
+export const AUTH_APP_UI_URL = process.env.AUTH_APP_UI_URL || 'http://localhost:3000';
